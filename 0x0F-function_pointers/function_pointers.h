82f68710713793b85f81bfed67100b0cfd194a9e
curhvyn <michaelcurhvyn@gmail.com>
2022-05-17 15:09:19 -0700

    a function that prints a name.

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,4 +3,5 @@
 #include <stddef.h>
 int int_index(int *array, int size, int (*cmp)(int));
 void array_iterator(int *array, size_t size, void (*action)(int));
-void print
+void print_name(char *name, void (*f)(char *));
+#endif
