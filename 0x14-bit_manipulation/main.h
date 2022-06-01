9486c56aba13b62cb07565b4f0499067ad46f4d2
curhvyn <michaelcurhvyn@gmail.com>
2022-06-01 15:53:04 -0700

    a function that checks the endianness.

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -8,4 +8,6 @@ int get_bit(unsigned long int n, unsigned int index);
 int _putchar(char s);
 void print_binary(unsigned long int n);
 unsigned int binary_to_uint(const char *b);
+int get_endianness(void);
+
 #endif
